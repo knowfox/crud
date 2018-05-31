854c9cef8403bc4510902649dcf981cb08e90a76
Olav Schettler <olav@schettler.net>
2018-05-31 19:25:27 +0200

    Fixed display of checkbox

--- views/fields/checkbox.blade.php
+++ views/fields/checkbox.blade.php
@@ -1,7 +1,8 @@
 <div class="col-sm-{{ $field['cols'] or 6 }}{{ !empty($field['offs']) ? " offset-sm-{$field['offs']}" : '' }}">
     <div class="mt-4 checkbox">
         <label>
-            <input type="checkbox" name="{{ $name }}" value="1"> @lang($field['label'])
+            <input type="checkbox" name="{{ $name }}" value="1"
+                {!! !empty($entity->{$name}) ? 'checked="CHECKED"' : '' !!}> @lang($field['label'])
         </label>
     </div>
 </div>
