aaaf74f78962d59cddf493ce1f2d7cb8ebdda8c6
Olav Schettler <olav@schettler.net>
2019-01-03 01:31:11 +0100

    New field type "ref"

---
+++ views/uikit3/fields/ref.blade.php
@@ -0,0 +1,40 @@
+<div class="uk-width-{{ $field['width'] ?? '1-1' }}@m">
+    <div class="{{ $errors->has($name) ? ' uk-form-danger' : '' }}">
+        <label class="uk-form-label" for="{{ $name }}">{{ __($field['label']) }}</label>
+        <select name="{{ $name }}" id="ref-input-{{ $name }}" data-except="{{ $entity->id }}">
+            @if ($entity->$name)
+                <option value="{{ $entity->$name }}" selected="selected">{{ $field['ref_title']($entity) }}</option>
+            @endif
+        </select>
+    </div>
+</div>
+
+@push('scripts')
+    <script>
+        $('#ref-input-{{ $name }}').selectize({
+            persist: false,
+            valueField: 'id',
+            labelField: 'path',
+            searchField: 'title',
+            create: false,
+            load: function(query, callback) {
+                if (!query.length) return callback();
+                $.ajax({
+                    url: '{{ $field['url'] }}',
+                    type: 'GET',
+                    dataType: 'json',
+                    data: {
+                        except: $(this).get(0).$input.data('except'),
+                        q: query
+                    },
+                    error: function() {
+                        callback();
+                    },
+                    success: function(res) {
+                        callback(res.data);
+                    }
+                });
+            }
+        });
+    </script>
+@endpush
