c2f3855bcc3491d51d3a2fb6b4d7e9884a99552d
Olav Schettler <olav@schettler.net>
2019-02-01 13:05:37 +0100

    Ref field: Fields configurable for callback

--- views/uikit3/fields/ref.blade.php
+++ views/uikit3/fields/ref.blade.php
@@ -1,8 +1,8 @@
 <div class="uk-width-{{ $field['width'] ?? '1-1' }}@m">
     <div class="{{ $errors->has($name) ? ' uk-form-danger' : '' }}">
         <label class="uk-form-label" for="{{ $name }}">{{ __($field['label']) }}</label>
-        <select name="{{ $name }}" id="ref-input-{{ $name }}" data-except="{{ $entity->id }}">
-            @if ($entity->$name)
+        <select name="{{ $name }}" id="ref-input-{{ $name }}" data-except="{{ $entity->id ?? '' }}">
+            @if (!empty($entity->$name))
                 <option value="{{ $entity->$name }}" selected="selected">{{ $field['ref_title']($entity) }}</option>
             @endif
         </select>
@@ -14,8 +14,8 @@
         $('#ref-input-{{ $name }}').selectize({
             persist: false,
             valueField: 'id',
-            labelField: 'path',
-            searchField: 'title',
+            labelField: '{{ $field['label_field'] ?? 'title' }}',
+            searchField: '{{ $field['search_field'] ?? 'title' }}',
             create: false,
             load: function(query, callback) {
                 if (!query.length) return callback();
