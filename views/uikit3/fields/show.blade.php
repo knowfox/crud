5617844fbb20c444358bb35297b8c51eab830ddc
Olav Schettler <olav.schettler@dw.com>
2021-01-11 15:29:48 +0100

    show

--- views/uikit3/fields/show.blade.php
+++ views/uikit3/fields/show.blade.php
@@ -3,7 +3,7 @@ $value = !empty($entity->{$name}) ? $entity->{$name} : '';
 $type = is_numeric($value) ? 'number' : 'text';
 ?>
 
-@include('crud::fields.input', [
+@include('crud::' . $theme . '.fields.input', [
     'type' => $type,
     'attr' => 'readonly ',
 ])
